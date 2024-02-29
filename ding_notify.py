cbc25ad18ac15d192ccb5fe0724890afdeea21f8
IsshikiHugh <2511524185@qq.com>
2024-02-29 10:13:46 +0800

    update: support running exp

--- ding_notify.py
+++ ding_notify.py
@@ -1,7 +1,8 @@
 import os
+import sys
 import json
 import requests
-from omegaconf import OmegaConf
+# from omegaconf import OmegaConf
 
 class DingNotifier():
     def __init__(self, hook:str, secure_key=None, *args, **kwargs):
@@ -43,22 +44,30 @@ class DingNotifier():
         # 3. Return response dict.
         return resp_dict
 
-def build_notifier_from_cfg(cfg_path:str):
-    ''' Build DingNotifier from config file. '''
-    cfg = OmegaConf.load(cfg_path)
-    return DingNotifier(**cfg)
+# def build_notifier_from_cfg(cfg_path:str):
+#     ''' Build DingNotifier from config file. '''
+#     cfg = OmegaConf.load(cfg_path)
+#     return DingNotifier(**cfg)
 
 def build_notifier_from_env(): 
     ''' Build DingNotifier from env var. '''
     hook                     : str = os.getenv('DING_NOTIFIER_HOOK')
     sec_key                  : str = os.getenv('DING_NOTIFIER_SEC_KEY')
-    if not len(hook) > 0 or not hook.startswith("https://oapi.dingtalk.com/robot/send?access_token="):
+    if hook is None or not hook.startswith("https://oapi.dingtalk.com/robot/send?access_token="):
         raise ValueError(f'[DingNotify-ERROR] Invalid hook environment variable: $DING_NOTIFIER_HOOK = {hook}')
     if not len(sec_key) > 0:
         raise ValueError(f'[DingNotify-ERROR] Security key not set! $DING_NOTIFIER_SEC_KEY is empty!')
     return DingNotifier(hook, sec_key)
 
 if __name__ == '__main__':
-    # notifier = build_notifier_from_cfg('./config.yaml')
     notifier = build_notifier_from_env()
-    notifier.send_str('hello')
+
+    cmd = " ".join(sys.argv[1:])
+    time_s = os.popen('date').read().strip()
+    notifier.send_str(f"[{time_s} -> ]\nRunning experiment start using cmd:\n$ {cmd}")
+    
+    # run the command
+    os.system(cmd)
+    
+    time_e = os.popen('date').read().strip()
+    notifier.send_str(f"[{time_s} -> {time_e}]\nExperiment finished using cmd:\n$ {cmd}")
